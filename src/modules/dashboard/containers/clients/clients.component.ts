6da827a5f3ff329861c67d27a6095db1f3c6fdff
Louay sakhri <Louaysk723@gmail.com>
2020-06-03 15:08:18 +0100

    pagination commit

--- src/modules/dashboard/containers/clients/clients.component.ts
+++ src/modules/dashboard/containers/clients/clients.component.ts
@@ -3,7 +3,7 @@ import { CountryService } from '@modules/tables/services';
 import { Router } from '@angular/router';
 import { SBSortableHeaderDirective, SortEvent } from '@modules/tables/directives';
 import { Country } from '@modules/tables/models';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 
 
@@ -15,14 +15,16 @@ import { Observable } from 'rxjs';
   styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent implements OnInit {
-    @Input() pageSize = 4;
+    @Input() pageSize = 5;
     pageNumber : number = 1
+    pageSizeRange = [5,15,30]
     clients:any[] = []
     countries$!: Observable<Country[]>;
     total$!: Observable<number>;
     sortedColumn!: string;
     sortedDirection!: string;
     @ViewChildren(SBSortableHeaderDirective) headers!: QueryList<SBSortableHeaderDirective>;
+    paginationArray: number = 10;
 
 
   constructor( public countryService: CountryService,private route :Router,private changeDetectorRef: ChangeDetectorRef) {
@@ -47,21 +49,31 @@ export class ClientsComponent implements OnInit {
 
 
 onPageSizeChange(event){
+    debugger
     // console.log("event",event.split(": "[1]))
-    this.onGetClients(this.pageNumber,event[0])
+    this.onGetClients(this.pageNumber,event)
+    console.log("paginationArray : ",this.paginationArray)
 }
 
 onPageChange(event){
+
+    debugger
     this.onGetClients(event,this.pageSize)
+    console.log("paginationArray : ",this.paginationArray)
 }
 
   onGetClients(pageNumber = this.pageNumber,pageSize = this.pageSize){
     this.countryService.getclients(pageNumber,pageSize).subscribe(
         (res : any)=>{
+            console.log(res)
             this.clients= res.Items
-            this.countryService.page--
             this.countryService.page++
+            this.countryService.page--
+            console.clear
             console.log('data is ',this.clients)
+            // this.paginationArray = Array(clientsLength /pageSize);
+            this.paginationArray = res.TotalHits
+            console.log(this.paginationArray)
         }
     )
 }
