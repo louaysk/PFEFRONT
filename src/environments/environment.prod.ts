0e548d2bc22d57c2aacb6f7296d865b112ceabb0
Louay sakhri <Louaysk723@gmail.com>
2020-06-30 10:25:55 +0100

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:54277/api/'
-   // apiUrl : 'https://insomeabills.azurewebsites.net/api/'
+   // apiUrl: 'http://localhost:54277/api/'
+    apiUrl : 'https://insomeabills.azurewebsites.net/api/'
 };
